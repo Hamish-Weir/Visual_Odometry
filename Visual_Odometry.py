20b2828223ca2f2a72b912b516d393637dfa1525
c93350hw <hamish.weir@student.manchester.ac.uk>
2025-03-05 00:43:23 +0000

    made it into a class so i can do slam later

--- Visual_Odometry.py
+++ Visual_Odometry.py
@@ -4,48 +4,73 @@ import numpy as np
 import cv2 as cv
 import time
 
-def get_frame():
-    if cap.isOpened():
-        ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        exit(1)
-    else:
-        return ret, frame
+class Visual_Odometry():
+    def __init__(self, input_file):
+        self.orb = orb = cv.ORB_create()
+        self.bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False) #Brute Force Matcher
+        self.threshold = 0.5
+        self.cap = cv.VideoCapture(input_file)
 
-def get_matches(img1, img2):
+    def process_video(self):
+        prev_frame = self.get_frame()
+        prev_gray = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY)
 
-    # Initiate ORB detector
-    orb = cv.ORB_create() #IDK what and ORB is?
+        while self.cap.isOpened():
+            frame = self.get_frame()
 
-    # find the keypoints and descriptors with ORB
-    kp1, des1 = orb.detectAndCompute(img1,None)
-    kp2, des2 = orb.detectAndCompute(img2,None)
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    # create BFMatcher object
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False) #Brute Force Matcher
+            self.get_matches(prev_gray, gray)
 
-    # # Match descriptors.
-    # matches = bf.match(des1,des2)
 
-    # # Sort by distance.
-    # matches = sorted(matches, key = lambda x:x.distance)
+            prev_gray = gray
+            time.sleep(0.1)
+            
+            if cv.waitKey(1) == ord('q'):
+                break
+        
+        self.cap.release()
+        cv.destroyAllWindows()
 
-    matches = bf.knnMatch(des1, des2, k=2)
 
-    matches_vtb = []
-    sorted_matches = sorted(matches, key = lambda matchlist: kp1[matchlist[0].queryIdx].pt)
+    def get_frame(self):
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            exit(1)
+        else:
+            return frame
 
-    for n in sorted_matches:
-        n = sorted(n, key = lambda m:m.distance)
-        if len(n)>=2:
-            if ((n[0].distance/n[1].distance) < (0.5)):
-                matches_vtb.append(cv.DMatch(n[0].queryIdx,n[0].trainIdx,n[0].imgIdx,n[0].distance))   
+    def get_matches(self, img1, img2):
 
-    # Draw matches.
-    img3 = cv.drawMatches(img1,kp1,img2,kp2,matches_vtb,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    img3S = cv.resize(img3, (1280, 360)) 
-    cv.imshow(str(argv[1]),img3S)
+        # Initiate ORB detector
+         #IDK what and ORB is?
+
+        # find the keypoints and descriptors with ORB
+        kp1, des1 = self.orb.detectAndCompute(img1,None)
+        kp2, des2 = self.orb.detectAndCompute(img2,None)
+
+        # # Match descriptors.
+        # matches = self.bf.match(des1,des2)
+
+        # # Sort by distance.
+        # matches = sorted(matches, key = lambda x:x.distance)
+
+        matches = self.bf.knnMatch(des1, des2, k=2)
+
+        # Sort matches based on the keypoint positions in kp1
+        sorted_matches = sorted(matches, key=lambda m: kp1[m[0].queryIdx].pt)
+
+        matches_vtb = [
+            cv.DMatch(m[0].queryIdx, m[0].trainIdx, m[0].imgIdx, m[0].distance)
+            for m in sorted_matches if len(m) >= 2 and (m[0].distance / m[1].distance) < 0.5
+        ]
+
+        # # Draw matches.
+        img3 = cv.drawMatches(img1,kp1,img2,kp2,matches_vtb,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img3S = cv.resize(img3, (1280, 360)) 
+        cv.imshow(str(argv[1]),img3S)
 
     
 if __name__ == "__main__":
@@ -56,25 +81,5 @@ if __name__ == "__main__":
         print("Propper call is:")
         print("python3 Visual_Odometry.py [Input Video File] [Output File]\n")
     
-    cap = cv.VideoCapture(argv[1])
-    
-    prev_ret, prev_frame = get_frame()
-    prev_gray = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY)
-
-    while cap.isOpened():
-        ret, frame = get_frame()
-        
-
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # cv.imshow('Test', gray)
-        get_matches(prev_gray, gray)
-
-
-        prev_gray = gray
-        time.sleep(0.1)
-        if cv.waitKey(1) == ord('q'):
-            break
-    
-    cap.release()
-    cv.destroyAllWindows()
+    vo = Visual_Odometry(argv[1])
+    vo.process_video()
