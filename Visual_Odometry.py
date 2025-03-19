c6593c5d24c7a462018afe44ad52f45f222cd626
c93350hw <hamish.weir@student.manchester.ac.uk>
2025-03-19 16:34:08 +0000

    refactored VO Class and the Loader

--- Visual_Odometry.py
+++ Visual_Odometry.py
@@ -1,8 +1,11 @@
 import io
+import os
 from sys import argv
 import numpy as np
 import cv2 as cv
 import time
+from tqdm import tqdm
+from lib.visualisation import plotting
 
 class Visual_Odometry():
     def __init__(self, input_file):
@@ -10,42 +13,45 @@ class Visual_Odometry():
         self.bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False) #Brute Force Matcher
         self.threshold = 0.5
         self.cap = cv.VideoCapture(input_file)
+        self.videoLength = int(self.cap.get(cv.CAP_PROP_FRAME_COUNT))
+        self.images = self.get_frames()
 
-    def process_video(self):
-        prev_frame = self.get_frame()
-        prev_gray = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY)
 
-        while self.cap.isOpened():
-            frame = self.get_frame()
-
+    def play_video(self):
+        for i in range(0,self.videoLength):
+            frame = self.get_frame(i)
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-            self.get_matches(prev_gray, gray)
-
-
-            prev_gray = gray
-            time.sleep(0.1)
-            
+            time.sleep(0.05)
+            cv.imshow(str(argv[1]),frame)
             if cv.waitKey(1) == ord('q'):
                 break
-        
+
         self.cap.release()
         cv.destroyAllWindows()
 
-
-    def get_frame(self):
-        if self.cap.isOpened():
+    def get_frame(self, i):
+        return self.images[i]
+        
+    def get_frames(self):
+        if not self.cap.isOpened():
+            print("Error: Could not open video.")
+            return []
+        frame_list = []
+        while True:
             ret, frame = self.cap.read()
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            exit(1)
-        else:
-            return frame
+            if not ret:
+                break  # Stop when the video ends
+            frame_list.append(frame)
+        self.cap.release()
+        return frame_list
 
-    def get_matches(self, img1, img2):
+
+    def get_matches(self, i):
 
         # Initiate ORB detector
          #IDK what and ORB is?
+        img1 = self.get_frame(i-1)
+        img2 = self.get_frame(i)
 
         # find the keypoints and descriptors with ORB
         kp1, des1 = self.orb.detectAndCompute(img1,None)
@@ -67,19 +73,68 @@ class Visual_Odometry():
             for m in sorted_matches if len(m) >= 2 and (m[0].distance / m[1].distance) < 0.5
         ]
 
-        # # Draw matches.
-        img3 = cv.drawMatches(img1,kp1,img2,kp2,matches_vtb,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        img3S = cv.resize(img3, (1280, 360)) 
-        cv.imshow(str(argv[1]),img3S)
+        if show_matches:
+            img3 = cv.drawMatches(img1,kp1,img2,kp2,matches_vtb,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            img3S = cv.resize(img3, (1280, 360)) 
+            cv.imshow(str(argv[1]),img3S)
 
+        return kp1, kp2
+
+    def get_pose(self, kp1, kp2):
+        
+        pass
     
 if __name__ == "__main__":
 
     # Check Input
-    if (len(argv) != 3):
-        print("Incorrect number of arguments input.\n")
-        print("Propper call is:")
-        print("python3 Visual_Odometry.py [Input Video File] [Output File]\n")
+    if (len(argv) < 3):
+        print("Incorrect number of arguments input.")
+        print("     Propper call is:")
+        print("     python3 Visual_Odometry.py [Input Video File] [Output File] -Flags")
+        print("     Flags:")
+        print("         -H       Help")
+        print("         -M       Show Matches")
+        exit(1)
+
+    flags = argv[3:]
+
     
+    
+    if "-M" in flags:show_matches = True
+    else:show_matches = False
+    if "-V" in flags:show_video = True
+    else:show_video = False
+
     vo = Visual_Odometry(argv[1])
-    vo.process_video()
+
+    if show_video:
+        vo.play_video()
+
+    gt_path = []
+    estimated_path = []
+    # for i, gt_pose in enumerate(tqdm(vo.gt_poses, unit="pose")):
+    for i in range(vo.videoLength):
+        if i == 0:
+            # cur_pose = gt_pose
+
+            #Assume start straite, can be manualy changed later, idc
+            pose = "1.000000e+00 0.000000e+00 0.000000e+00 0.000000e+00 0.000000e+00 1.000000e+00 0.000000e+00 0.000000e+00 0.000000e+00 0.000000e+00 1.000000e+00 0.000000e+00"
+            T = np.fromstring(pose, dtype=np.float64, sep=' ')
+            T = T.reshape(3, 4)
+            T = np.vstack((T, [0, 0, 0, 1]))
+            cur_pose = T
+        else:
+            kp1, kp2 = vo.get_matches(i)
+            transf = vo.get_pose(kp1, kp2)
+            cur_pose = np.matmul(cur_pose, np.linalg.inv(transf))
+            # print ("\nGround truth pose:\n" + str(gt_pose))
+            print ("\n Current pose:\n" + str(cur_pose))
+            print ("The current pose used x,y: \n" + str(cur_pose[0,3]) + "   " + str(cur_pose[2,3]) )
+        # gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
+        estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+        
+  
+    
+    plotting.visualize_paths(gt_path, estimated_path, "Visual Odometry", file_out=os.path.basename(argv[2]) + ".html")
+
+    
